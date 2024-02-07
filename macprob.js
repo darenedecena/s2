10ccb9aeb56800f1d8ce35ba5eeab7be809b66b1
darenedecena <157150962+darenedecena@users.noreply.github.com>
2024-02-07 22:24:43 +0800

    macprob.js

--- macprob.js
+++ macprob.js
@@ -1,26 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll(".day-button");
-    const squares = document.querySelectorAll(".square");
-  
-    buttons.forEach(function (button, index) {
-      button.addEventListener("click", function () {
-        squares.forEach(function (square) {
-          square.classList.remove("falling");
-        });
-  
-        squares[index].classList.add("falling");
-  
-        setTimeout(function () {
-          squares[index].style.backgroundColor = "#ffffff";
-        }, 100);
-  
-        if (index > 0) {
-          setTimeout(function () {
-            squares[index - 1].style.backgroundColor = squares[index - 1].style.borderColor;
-            squares[index - 1].classList.remove("falling");
-          }, 1000);
-        }
-      });
-    });
-  });
-  
+var currentColor = null;
+var previousColor = null;
+var activeBox = "big-box1";
+
+function copyColor(day) {
+  var bigBox1 = document.getElementById("big-box1");
+  var bigBox2 = document.getElementById("big-box2");
+  var button = document.querySelector("." + day);
+  var newColor = window.getComputedStyle(button).getPropertyValue('background-color');
+
+  currentColor = newColor;
+
+ 
+  var activeBoxElement = activeBox === "big-box1" ? bigBox1 : bigBox2;
+  var inactiveBoxElement = activeBox === "big-box1" ? bigBox2 : bigBox1;
+  activeBoxElement.style.borderColor = newColor;
+  activeBoxElement.style.transition = "transform 1s ease-in-out";
+  activeBoxElement.style.transform = "translateY(0%)"; 
+  activeBoxElement.style.opacity = 1;
+
+  inactiveBoxElement.style.opacity = 0;
+  inactiveBoxElement.style.transition = "transform 1s ease-in-out, opacity 1s ease-in-out";
+  inactiveBoxElement.style.transform = "translateY(-100%)";
+
+  previousColor = newColor;
+  activeBox = activeBox === "big-box1" ? "big-box2" : "big-box1";
+}
